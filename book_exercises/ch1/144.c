6d608cf13e9e941dcc74e27a545128d99c2f175e
martin <martin@mandella.se>
2020-04-29 16:31:56 +0200

    solved exercis 144

---
+++ book_exercises/ch1/144.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void next_int() {
+	int digit;
+	printf("Enter an integer: ");
+	scanf("%d", &digit);
+}
+
+void next_double() {
+	double number;
+	printf("Enter a double: ");
+	scanf("%lf", &number);
+}
