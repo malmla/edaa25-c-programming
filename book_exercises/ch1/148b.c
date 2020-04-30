f1af6b2241e547fb3bfc1f7d488841a7ff925738
martin <martin@mandella.se>
2020-04-30 16:00:19 +0200

    solved ex 148

---
+++ book_exercises/ch1/148b.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void input(double a[], size_t size)
+{
+	int e;
+	double in = 0;
+	for(int i = 0; i < size; i++)
+	{
+		printf("Input next element: \n");
+		e = scanf("%lf", &in);
+		a[i] = in;
+		if (e != 1)
+			printf("input went wrong\n");
+	}
+}
+
+void output(double a[], size_t size)
+{
+	for(int i = 0; i < size; i++)
+	{
+		printf("element %u is %lf\n", i, a[i]);
+	}
+}
+
+/*
+ * Reads size doubles from input and puts them in an array. Using pointers instead of brackets.
+ */
+
+int main(void)
+{
+	size_t size = 0;
+	printf("How many inputs are you putting in?\n");
+	int arguments = scanf("%zu", &size);
+	if (arguments != 1)
+		printf("input went wrong\n");
+	double a[size];
+	input(a, size);
+	output(a, size);
+}
